885748c5fb191ff694e0eebf32ea518442fbcfc9
Minhyeok Son <124177883+Marcus-Son@users.noreply.github.com>
2025-05-04 00:39:48 +0900

    Delete Easy/1251. Average Selling Price directory

--- Easy/1251. Average Selling Price/solution.sql
+++
@@ -1,5 +0,0 @@
-SELECT p.product_id, ROUND(CAST(SUM(price*units) AS NUMERIC)/SUM(units),2) AS average_price
-FROM Prices p
-LEFT JOIN UnitsSold u
-ON p.product_id=u.product_id AND purchase_date BETWEEN start_date AND end_date
-GROUP BY p.product_id;
