9eb5a57399011e6a793447c530955405a90526c0
Minhyeok Son <124177883+Marcus-Son@users.noreply.github.com>
2025-05-07 18:02:10 +0900

    Update solution.sql

--- Easy/1633. Percentage of Users Attended a Contest/solution.sql
+++ Easy/1633. Percentage of Users Attended a Contest/solution.sql
@@ -1,8 +1,5 @@
-SELECT contest_id, percentage
-FROM(
-    SELECT contest_id, 
-ROUND(COUNT(DISTINCT(user_id))*100.0/(SELECT COUNT(*) AS total_count FROM Users),2) AS percentage
+SELECT r.contest_id, 
+ROUND(COUNT(DISTINCT(user_id))*100.0/(SELECT COUNT(*) FROM Users),2) AS percentage
 FROM Register r
-GROUP BY contest_id
-)
+GROUP BY r.contest_id
 ORDER BY percentage DESC, contest_id ASC;
